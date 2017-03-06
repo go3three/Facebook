1b5cb76ea9076d498878262b557a5d92fd95d2b0
besart <besartshyti@gmail.com>
2017-03-06 17:12:02 +0200

    fix errors, convert string to object usiing querystring

--- app/config/conn.js
+++ app/config/conn.js
@@ -2,22 +2,23 @@
 
 var pg = require('pg');
 var config = {
-    user: 'postgres', //env var: PGUSER
+    // user: 'postgres', //env var: PGUSER
     database: 'facebook', //env var: PGDATABASE
-    password: '482106', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    // password: '482106', //env var: PGPASSWORD
+    // host: 'localhost', // Server hosting the postgres database
+    // port: 5432, //env var: PGPORT
+    // max: 10, // max number of clients in the pool
+    // idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 var client = new pg.Client(config);
 
 client.connect(function(err) {
     if (err) throw err;
 
-    client.query('Create table if not exists info (id SERIAL, first_name varchar(50),last_name varchar(50)\
-                ,dob date,gender char(2),about text,image_url text ,firends int,username\
-                 varchar(25),password varchar(50),primary key(id));', function(errorWrite, result) {
+    client.query(`Create table if not exists info 
+      (id SERIAL, first_name varchar(50),last_name varchar(50)
+                ,dob date,gender char(2),about text,image_url text ,
+                firends int,username varchar(25),password varchar(50),primary key(id));`, function(errorWrite, result) {
         if (errorWrite) {
             console.log('errorWrite', errorWrite);
         }
--- app/profile.js
+++ app/profile.js
@@ -1,35 +1,49 @@
 var client = require('./config/conn.js');
 var utils = require('./utils.js');
-
+var qs = require('querystring')
 module.exports = function(req, res) {
 
     utils.parseBody(req, function(err, body) {
 
         // res.end(JSON.stringify(body));
-console.log(JSON.parse(body));
+var b=qs.parse(body);
 
-client.query("insert into info (first_name,last_name,dob,gender,about,image_url,firends,username,password)\
- values('ahmed','ibrahim','06/10/1986','M','software engineering','www.google.com',3,'');",
-    function(errorWrite, result) {
+client.query(`
+  insert into
+    info (
+      first_name,
+      last_name,
+      dob,
+      gender,
+      about,
+      image_url,
+      firends,
+      username,
+      password
+    )
+    values(
+      'ahmed',
+      'ibrahim',
+      '06/10/1986',
+      'M',
+      'software engineering',
+      'www.google.com',
+      '3',
+      '${b.username}',
+      '${b.password}'
+    )
+  ;`, function(errorWrite, result) {
         if (errorWrite) {
             console.log('errorWrite', errorWrite);
         }
-
-
+        console.log('result',result);
+        client.query("SELECT * FROM info", function(err, row) {
+          console.log('row',row);
+          var r = [];
+      //  res.end('ok?');
+           r.push(JSON.stringify(row))
+           res.end(JSON.stringify(r))
+        });
     });
-
   });
-// var r=sel(function(b){
-// b.rows.maps(function(ele){
-//
-//
-// })
-//   return b});
-client.query("select * from info where id =4").on('row', function(row) {
-    var r = [];
-    r.push(JSON.stringify(row))
-    res.end(JSON.stringify(r))
-});
-
-
 }
