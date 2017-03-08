8366ad400074d708cf8800cdf82a7796840f8afc
Mohammed Naji <moha.buy@gmail.com>
2017-03-08 14:40:20 +0200

    finish the code

--- app/config/conn.js
+++ app/config/conn.js
@@ -10,16 +10,16 @@ var config = {
     idleTimeoutMillis: 30000,
     ssl:true // how long a client is allowed to remain idle before being closed
 };
-var config = {
-    user: 'postgres', //env var: PGUSER
-    database: 'facebook', //env var: PGDATABASE
-    password: '482106', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000,
-    ssl:true // how long a client is allowed to remain idle before being closed
-};
+// var config = {
+//     user: 'postgres', //env var: PGUSER
+//     database: 'facebook', //env var: PGDATABASE
+//     password: '482106', //env var: PGPASSWORD
+//     host: 'localhost', // Server hosting the postgres database
+//     port: 5432, //env var: PGPORT
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000,
+//     ssl:true // how long a client is allowed to remain idle before being closed
+// };
 var createClient = (config, cb) => {
     var client = new pg.Client(config);
     client.connect(err => {
