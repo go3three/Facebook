c3bbd8002b157ca7febce1d0897bbc2888c29d3b
Mohammed Naji <moha.buy@gmail.com>
2017-03-09 11:14:20 +0200

    add more test

--- tests/db.test.js
+++ tests/db.test.js
@@ -4,8 +4,8 @@ var shot = require('shot');
 var db = require('../app/config/conn.js');
 var config = {
     user: 'postgres', //env var: PGUSER
-    database: 'facebook', //env var: PGDATABASE
-    password: '330167', //env var: PGPASSWORD
+    database: 'test', //env var: PGDATABASE
+    password: '482106', //env var: PGPASSWORD
     host: 'localhost', // Server hosting the postgres database
     port: 5432, //env var: PGPORT
     max: 10, // max number of clients in the pool
@@ -20,7 +20,15 @@ test('db.createTable(): should select data from the table info', t => {
     t.end();
   });
 });
-
+test('db.insertdata(): insert data in info table', t => {
+  db.insertdata(client,`INSERT INTO info (first_name,last_name,dob,gender,about,image_url,friends,username,password) VALUES ('test','test','11-11-2000','m','','www.google.com','0','test','test');`,(errInsert, resInsert) => {
+    t.notOk(errInsert, 'no errors');
+    t.ok(resInsert, 'got res result');
+    client.end(() => {
+      t.end();
+    });
+  });
+});
 test('finish database client', t => {
   client.end(() => {
     t.pass('done, database down');
